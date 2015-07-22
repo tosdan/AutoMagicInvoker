3bd2b8a49b6bfc652d7d57e35f40f079d604d676
tosdan <stdanielets@gmail.com>
2015-07-22 02:07:23 +0200

    fix accidentally deleted classes

--- WEB-INF/src/com/github/tosdan/autominvk/render/Mime.java
+++
@@ -1,9 +0,0 @@
-package com.github.tosdan.autominvk.render;
-
-public class Mime {
-
-	public static final String TEXT_HTML = "text/html";
-	public static final String TEXT_PLAIN = "text/plain";
-	public static final String TEXT_JAVASCRIPT = "text/javascript";
-
-}
