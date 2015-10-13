94db60cd9df5165481c592fda491f55bf9e25831
tosdan <stdanielets@gmail.com>
2015-10-13 22:54:15 +0200

    Added element gsonDateFormat

--- WEB-INF/src/com/github/tosdan/autominvk/IamInvokableAction.java
+++ WEB-INF/src/com/github/tosdan/autominvk/IamInvokableAction.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.text.SimpleDateFormat;
 
 import com.github.tosdan.autominvk.rendering.AutoMagicRender;
 import com.github.tosdan.autominvk.rendering.render.DefaultNull;
@@ -17,11 +18,33 @@ import com.github.tosdan.autominvk.rendering.render.DefaultNull;
 @Target(ElementType.METHOD)
 public @interface IamInvokableAction {
 
+	/**
+	 * Content type da usare nella response.
+	 * @return
+	 */
 	String mime() default "";
-	
+	/**
+	 * Metodo della request accettato (GET, POST, DELETE, PUT, HEAD): se diverso da quanto specificato restituisce un errore.
+	 * @return
+	 */
 	String reqMethod() default "";
-	
+	/**
+	 * Alternativa cablata per identificare il metodo da chiamare. Invece di usare l'uri convenzionale formato dal 
+	 * package più la classe più il nome del metodo, si può utilizzare un alias completamente arbitrario.
+	 * @return
+	 */
 	String alias() default "";
-	
+	/**
+	 * Formato data con cui verrà fatto il parse dei parametri che devono essere trasformati in oggetti {@link java.util.Date}.
+	 * <br>Per i formati disponibili fare riferimento a {@link SimpleDateFormat}. 
+	 * <br>Il formato di default è <code>dd/MM/yyyy</code>
+	 * @return
+	 */
+	String gsonDateFormat() default "dd/MM/yyyy";
+	/**
+	 * Classe, che implementa {@link AutoMagicRender}, da usare per effettuare il render dell'oggetto 
+	 * restituito dal metodo chiamato. 
+	 * @return
+	 */
 	Class<? extends AutoMagicRender> render() default DefaultNull.class;
 }
