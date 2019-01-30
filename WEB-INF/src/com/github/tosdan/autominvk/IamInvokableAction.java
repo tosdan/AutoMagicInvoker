adc07df3674d158f27941b50da79c7f508e1ba85
tosdan <stdanielets@gmail.com>
2019-01-30 09:14:07 +0100

    serializzazione date nel formato standard per il render Jjson2

--- WEB-INF/src/com/github/tosdan/autominvk/IamInvokableAction.java
+++ WEB-INF/src/com/github/tosdan/autominvk/IamInvokableAction.java
@@ -37,10 +37,9 @@ public @interface IamInvokableAction {
 	/**
 	 * Formato data con cui verrà fatto il parse dei parametri che devono essere trasformati in oggetti {@link java.util.Date}.
 	 * <br>Per i formati disponibili fare riferimento a {@link SimpleDateFormat}. 
-	 * <br>Il formato di default è <code>dd/MM/yyyy</code>. Per annullare il default impostare con una stringa vuota.
 	 * @return
 	 */
-	String gsonDateFormat() default "dd/MM/yyyy";
+	String gsonDateFormat() default "";
 	/**
 	 * Formato orario con cui verrà fatto il parse dei parametri che devono essere trasformati in oggetti {@link java.sql.Time}.
 	 * <br>Per i formati disponibili fare riferimento a {@link SimpleDateFormat}. 
